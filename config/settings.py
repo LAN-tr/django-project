9b84db0f545c86a8a4acee49ad2b8fc25ac33119
Lucy Norman <lucynorman@mac.com>
2024-11-12 09:53:36 +0000

    work through deploying on heroku

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#8am4%4eg(ew)ombhx0byrql)b&u%9(z36osrbu=iv04@2qgc8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-lantr-djangoproject-vnp6vd693lw.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-lantr-djangoproject-vnp6vd693lw.ws.codeinstitute-ide.net',
+                '.heroku.com']
 
 
 # Application definition
