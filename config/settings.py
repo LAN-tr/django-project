5e6954829f9a29fc796949741cff3f7df56a69d5
Lucy Norman <lucynorman@mac.com>
2024-11-12 10:46:29 +0000

    changed debug value

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#8am4%4eg(ew)ombhx0byrql)b&u%9(z36osrbu=iv04@2qgc8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lantr-djangoproject-vnp6vd693lw.ws.codeinstitute-ide.net', '.heroku.com']
 
